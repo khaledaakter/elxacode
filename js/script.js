058298a4a84cadfbd8ce51f7bfff60afda2ff700
unknown <khaledanahar43@gmail.com>
2019-04-29 12:57:47 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $('.sky-nav-right li a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 3000);
+       return false;
+      }
+    }
+  });
+ 
+});
+$(document).ready(function(){
+  $('.companypage-menu li a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 3000);
+       return false;
+      }
+    }
+  });
+ 
+});
+
+
+
+
+        
